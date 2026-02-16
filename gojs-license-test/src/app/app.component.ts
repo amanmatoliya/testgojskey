efd9acc54b0220341891467fb882951b3da1fff2
Aman Matoliya <aman007matoliya@gmail.com>
2026-02-16 19:52:08 +0530

    main key pick

--- gojs-license-test/src/app/app.component.ts
+++ gojs-license-test/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('diagramDiv') diagramDiv: ElementRef;
   
   // Set your GoJS license key here
-  private licenseKey = '2b8647e1b56649c702d90776423d68f919a175639d811fa30e0414f3b90d3d06329fbb2855d38d9387a81bfe497d90d1dcc03b209148023cb731d7d94be08eaae53327e5470b4489a20b24969cff7ff2ff7872f3c1b024f2d36a9cf4bef8c59c0eb8f2c6589d08bb2b28';
+  //private licenseKey = '2b8647e1b56649c702d90776423d68f919a175639d811fa30e0414f3b90d3d06329fbb2855d38d9387a81bfe497d90d1dcc03b209148023cb731d7d94be08eaae53327e5470b4489a20b24969cff7ff2ff7872f3c1b024f2d36a9cf4bef8c59c0eb8f2c6589d08bb2b28';
 
   ngAfterViewInit(): void {
     this.initializeDiagram();
@@ -18,7 +18,7 @@ export class AppComponent implements AfterViewInit {
 
   initializeDiagram(): void {
     // Set the license key
-    go.Diagram.licenseKey = this.licenseKey;
+    //go.Diagram.licenseKey = this.licenseKey;
 
     // Create the Diagram
     const $ = go.GraphObject.make;
